f64d34b21ac9480198878d15b49a1e513d2f31cc
ab21055-svg <ab21055@shibaura-it.ac.jp>
2025-09-25 13:57:35 +0900

    create calculator.py
    クラスの作成
    足し算・引き算の実践

---
+++ calculator.py
@@ -0,0 +1,14 @@
+class calculator:
+    def add(a,b):
+        return a+b
+    
+    def sub(a,b):
+        return a-b
+
+if __name__=='__main__':
+    numa=23
+    numb=22
+
+print(calculator.add(numa,numb))
+print(calculator.sub(numa,numb))
+
